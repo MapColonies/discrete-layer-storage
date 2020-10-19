8e819205ab384c4c51cc1bbb8d15c55406f46978
Shahar s <cl_shaharshavit@rnd-hub.com>
2020-10-19 11:44:03 +0300

    feat(test): added date search tests

--- src/DAL/ImageDataRepository.ts
+++ src/DAL/ImageDataRepository.ts
@@ -7,11 +7,9 @@ import { OrderFiled, SearchOptions } from '../models/searchOptions';
 
 @EntityRepository(ImageData)
 export class ImageDataRepository extends Repository<ImageData> {
-
   private readonly mcLogger: MCLogger; //don't override internal repository logger.
 
-  public constructor(
-  ) {
+  public constructor() {
     super();
     this.mcLogger = container.resolve(MCLogger); //direct injection don't work here due to being initialized by typeOrm
   }
@@ -26,7 +24,9 @@ export class ImageDataRepository extends Repository<ImageData> {
       this.mcLogger.info(`creating ImageData record with id: ${image.id}`);
       await this.save(image);
     } else {
-      this.mcLogger.info(`duplicate value error has occurred when creating image "${image.id}"`);
+      this.mcLogger.info(
+        `duplicate value error has occurred when creating image "${image.id}"`
+      );
       //TODO: replace with custom exception type and handle in service
       throw new Error('duplicate entry inserted');
     }
@@ -38,7 +38,9 @@ export class ImageDataRepository extends Repository<ImageData> {
       this.mcLogger.info(`updating ImageData record with id: ${image.id}`);
       await this.save(image);
     } else {
-      this.mcLogger.info(`attempt to update non existing record "${image.id}" has occurred`);
+      this.mcLogger.info(
+        `attempt to update non existing record "${image.id}" has occurred`
+      );
       //TODO: replace with custom exception type and handle in service
       throw new Error('invalid update');
     }
@@ -50,61 +52,86 @@ export class ImageDataRepository extends Repository<ImageData> {
       this.mcLogger.info(`deleting ImageData record with id: ${image.id}`);
       await this.delete({ id: id });
     } else {
-      this.mcLogger.info(`attempt to delete non existing record "${id}" has occurred`);
+      this.mcLogger.info(
+        `attempt to delete non existing record "${id}" has occurred`
+      );
       //TODO: replace with custom exception type and handle in service
       throw new Error('image to delete was not found');
     }
   }
 
-  public async search(options: SearchOptions) : Promise<ImageData[]>
-  {
-    //TODO: add res order: asc / desc
+  public async search(options: SearchOptions): Promise<ImageData[]> {
     let builder = this.createQueryBuilder('image');
-    let first=true;
-    //add filters
-    if (options.geometry){
-      builder = this.addFootprintFilter(builder,options.geometry,first);
-      first = false;
-    }
-    if (options.startDate){
-      builder = this.addStartDate(builder,options.startDate,first);
-      first = false;
-    }
-    if (options.endDate){
-      builder = this.addEndDate(builder,options.endDate,first);
-      first = false;
-    }
-
-    //add order
-    let order: "ASC" | "DESC" = "ASC"
-    let orderBy = OrderFiled.DATE;
-    if(options.sort){
-      if(options.sort.desc){
-        order = "DESC"
-      }
-      orderBy = options.sort.orderBy;
-    }
-    builder = builder.orderBy(orderBy.toString(),order);
+    builder = this.addSearchFilters(options, builder);
+    builder = this.addSearchOrder(options, builder);
     //TODO: add pagination and limit result size
     return builder.getMany();
   }
 
-  private addFootprintFilter(builder:SelectQueryBuilder<ImageData>, geometry:Geometry,first: boolean):SelectQueryBuilder<ImageData>{
-    const filter = 'ST_Intersects(image.footprint, ST_SetSRID(ST_GeomFromGeoJSON(:geometry),4326))';
-    builder = first ? builder.where(filter) : builder.andWhere(filter);
-    return builder.setParameter('geometry',geometry);
+  private addSearchFilters(
+    options: SearchOptions,
+    builder: SelectQueryBuilder<ImageData>
+  ): SelectQueryBuilder<ImageData> {
+    let first = true;
+    if (options.geometry) {
+      builder = this.addFootprintFilter(builder, options.geometry, first);
+      first = false;
+    }
+    if (options.startDate) {
+      builder = this.addStartDate(builder, options.startDate, first);
+      first = false;
+    }
+    if (options.endDate) {
+      builder = this.addEndDate(builder, options.endDate, first);
+      first = false;
+    }
+    return builder;
   }
 
-  private addStartDate(builder:SelectQueryBuilder<ImageData>, startDate:Date,first: boolean):SelectQueryBuilder<ImageData>{
+  private addSearchOrder(
+    options: SearchOptions,
+    builder: SelectQueryBuilder<ImageData>
+  ): SelectQueryBuilder<ImageData> {
+    let order: 'ASC' | 'DESC' = 'ASC';
+    let orderBy = OrderFiled.DATE;
+    if (options.sort) {
+      if (options.sort.desc) {
+        order = 'DESC';
+      }
+      orderBy = options.sort.orderBy;
+    }
+    builder = builder.orderBy(orderBy.toString(), order);
+    return builder;
+  }
+
+  private addFootprintFilter(
+    builder: SelectQueryBuilder<ImageData>,
+    geometry: Geometry,
+    first: boolean
+  ): SelectQueryBuilder<ImageData> {
+    const filter =
+      'ST_Intersects(image.footprint, ST_SetSRID(ST_GeomFromGeoJSON(:geometry),4326))';
+    builder = first ? builder.where(filter) : builder.andWhere(filter);
+    return builder.setParameter('geometry', geometry);
+  }
+
+  private addStartDate(
+    builder: SelectQueryBuilder<ImageData>,
+    startDate: Date,
+    first: boolean
+  ): SelectQueryBuilder<ImageData> {
     const filter = 'image.date >= :startDate';
     builder = first ? builder.where(filter) : builder.andWhere(filter);
-    return builder.setParameter('startDate',startDate);
+    return builder.setParameter('startDate', startDate);
   }
 
-  private addEndDate(builder:SelectQueryBuilder<ImageData>, endDate:Date,first: boolean):SelectQueryBuilder<ImageData>{
+  private addEndDate(
+    builder: SelectQueryBuilder<ImageData>,
+    endDate: Date,
+    first: boolean
+  ): SelectQueryBuilder<ImageData> {
     const filter = 'image.date <= :endDate';
     builder = first ? builder.where(filter) : builder.andWhere(filter);
-    return builder.setParameter('endDate',endDate);
+    return builder.setParameter('endDate', endDate);
   }
-
 }
